8a7b616472eed9f564ac88885a89de4de01fb39d
kalt <kalt>
1997-06-18 17:14:51 +0000

    better version. will soon be replaced anyway

--- support/rev.sh
+++ support/rev.sh
@@ -1,7 +1,19 @@
-rev=`uname -h 2>&1 | grep usage | grep p | awk ' { print $1 } ' -`
-if [ x$rev = x ] ; then
-	revdir=`uname -s`-`uname -r|sed -e 's/\([^-]*\).*/\1/'`
+#! /bin/sh
+#
+# $Id: rev.sh,v 1.2 1997/06/18 17:14:51 kalt Exp $
+# 
+
+if test "`uname -s`" = AIX
+then
+  revdir="AIX-`oslevel 2>/dev/null|sed -e \"s/<//g\" -e \"s/>//g\" -e
+\"s@/@-@g\"`"
 else
-	revdir=`uname -p`-`uname -s`-`uname -r|sed -e 's/\([^-]*\).*/\1/'`
+  uname -p >/dev/null 2>&1
+  if test "$?" != 0
+  then
+    revdir="`uname -s`-`uname -r|sed -e \"s/\\([^-]*\\).*/\1/\" -e \"s@/@-@g\"`"
+  else
+    revdir="`uname -p`-`uname -s`-`uname -r|sed -e \"s/\\([^-]*\\).*/\1/\" -e \"s@/@-@g\"`"
+  fi
 fi
-echo "$revdir" | sed -e 's@/@-@g'
+echo "$revdir"
