4ebcb6e66cbabcb45cc3354f95ade36aa43f393c
kalt <kalt>
1997-06-27 18:53:40 +0000

    buffer overflow

--- ircd/s_serv.c
+++ ircd/s_serv.c
@@ -22,7 +22,7 @@
  */
 
 #ifndef lint
-static  char rcsid[] = "@(#)$Id: s_serv.c,v 1.15 1997/06/09 14:50:16 kalt Exp $";
+static  char rcsid[] = "@(#)$Id: s_serv.c,v 1.16 1997/06/27 18:53:40 kalt Exp $";
 #endif
 
 #include <sys/types.h>
@@ -332,9 +332,9 @@ char	*parv[];
 	else if (parc > 2)
 	    {
 		(void)strncpy(info, parv[2], REALLEN);
-		if (parc > 3)
+		i = strlen(info);
+		if (parc > 3 && ((i+2) > REALLEN))
 		    {
-				i = strlen(info);
 				(void)strncat(info, " ", REALLEN - i - 1);
 				(void)strncat(info, parv[3], REALLEN - i - 2);
 		    }
