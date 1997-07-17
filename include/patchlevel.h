e2a58ad1536e4893c5a4da3da3b865db49d47037
kalt <kalt>
1997-07-17 01:36:45 +0000

    v2_9_3b21

--- include/patchlevel.h
+++ include/patchlevel.h
@@ -17,6 +17,6 @@
  */
 
 #ifndef	PATCHLEVEL
-#define PATCHLEVEL "0209022000"		/* for server identification */
+#define PATCHLEVEL "0209022100"		/* for server identification */
 #define DEVLEVEL 'b'
 #endif
