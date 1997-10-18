fa42a81f723b11328845f22d91a6f8a8685f05fb
kalt <kalt>
1997-10-18 13:43:20 +0000

    &DEBUG is now +s instead of +p so it doesn't show up in /list (sick and tired of ppl asking about it)

--- ircd/channel.c
+++ ircd/channel.c
@@ -32,7 +32,7 @@
  */
 
 #ifndef	lint
-static	char rcsid[] = "@(#)$Id: channel.c,v 1.17 1997/10/13 17:39:58 kalt Exp $";
+static	char rcsid[] = "@(#)$Id: channel.c,v 1.18 1997/10/18 13:43:20 kalt Exp $";
 #endif
 
 #include "os.h"
@@ -516,7 +516,7 @@ aClient	*mp;
 	chptr = get_channel(mp, "&DEBUG", CREATE);
 	strcpy(chptr->topic, "SERVER MESSAGES: debug messages [you shouldn't be here! ;)]");
 	add_user_to_channel(chptr, mp, CHFL_CHANOP);
-	chptr->mode.mode = smode|MODE_PRIVATE;
+	chptr->mode.mode = smode|MODE_SECRET;
 
 	setup_svchans();
 }
