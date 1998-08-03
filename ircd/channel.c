3309e33c3f3801561d6d320a1686627e6f43f09b
kalt <kalt>
1998-08-03 12:13:34 +0000

    server reops are limited to !channels

--- ircd/channel.c
+++ ircd/channel.c
@@ -32,7 +32,7 @@
  */
 
 #ifndef	lint
-static	char rcsid[] = "@(#)$Id: channel.c,v 1.53 1998/08/03 01:10:19 kalt Exp $";
+static	char rcsid[] = "@(#)$Id: channel.c,v 1.54 1998/08/03 12:13:34 kalt Exp $";
 #endif
 
 #include "os.h"
@@ -3120,7 +3120,7 @@ time_t	now;
 		else
 		    {
 			cur_nb++;
-			if (chptr->reop <= now)
+			if (*chptr->chname == '!' && chptr->reop <= now)
 				r_cnt += reop_channel(now, chptr);
 		    }
 		chptr = chptr->nextch;
