e39377c5094d846e2bc817e18a7fac76de49ef3f
kalt <kalt>
1998-09-13 16:50:36 +0000

    v2_10b12

--- common/patchlevel.h
+++ common/patchlevel.h
@@ -17,6 +17,6 @@
  */
 
 #ifndef	PATCHLEVEL
-#define PATCHLEVEL "0209991100"		/* for server identification */
+#define PATCHLEVEL "0209991200"		/* for server identification */
 #define DEVLEVEL 'b'
 #endif
