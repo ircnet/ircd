b123b0acd3783da04a5603b44ed9699125e012dd
kalt <kalt>
1998-09-24 17:33:46 +0000

    poll() cannot be detected by configure safely until we're sure it works everywhere. (it doesn't on linux 2.0.x old libc for instance) reverted earlier changes, configure still checks for poll() but uses the finding only to display a message.

--- common/os.h
+++ common/os.h
@@ -543,9 +543,11 @@ extern struct hostent *_switch_gethostbyname_r __P((const char *name,
 # define HAVE_GETRUSAGE 1
 #endif
 
+/* linux 2.0.x has poll(), compiling works, but things don't run well
 #if defined(HAVE_POLL)
 # define USE_POLL 1
 #endif
+*/
 
 /*  select portability problems - some systems do not define FD_... macros; on
  *  some systems (for example HPUX), select uses an int * instead of an
--- support/acconfig.h
+++ support/acconfig.h
@@ -1,4 +1,4 @@
-/* $Id: acconfig.h,v 1.6 1997/10/13 17:42:28 kalt Exp $ */
+/* $Id: acconfig.h,v 1.7 1998/09/24 17:34:47 kalt Exp $ */
 
 /* Define if zlib package must be used for compilation/linking. */
 #undef USE_ZLIB
@@ -63,6 +63,7 @@
 #undef H_ERRNO_DECLARED
 
 /* Define if poll(2) must be used instead of select(2). */
+/* Note: some systems (e.g. linux 2.0.x) have a non-working poll() */
 #undef USE_POLL
 
 /* Define if the system provides POSIX sigaction. */
