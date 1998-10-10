629bfd485ce4713921414138397f2bff5ddfcb74
kalt <kalt>
1998-10-10 10:19:41 +0000

    match_modeid() used cptr->ip for remote clients as well as local ones

--- ircd/channel.c
+++ ircd/channel.c
@@ -32,7 +32,7 @@
  */
 
 #ifndef	lint
-static	char rcsid[] = "@(#)$Id: channel.c,v 1.69 1998/10/08 17:46:20 kalt Exp $";
+static	char rcsid[] = "@(#)$Id: channel.c,v 1.70 1998/10/10 10:19:41 kalt Exp $";
 #endif
 
 #include "os.h"
@@ -266,9 +266,12 @@ aChannel *chptr;
 
 	if (!tmp)
 	    {
-		char *ip = (char *) inetntoa((char *)&cptr->ip);
+		char *ip = NULL;
 
-		if (strcmp(ip, cptr->user->host))
+		if (MyConnect(cptr))
+			ip = (char *) inetntoa((char *)&cptr->ip);
+
+		if (ip == NULL || strcmp(ip, cptr->user->host))
 		    {
 			s = make_nick_user_host(cptr->name,
 						cptr->user->username, ip);
