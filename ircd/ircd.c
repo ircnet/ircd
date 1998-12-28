cd767796c7e75f860b25682d4580d047bfe8dc60
kalt <kalt>
1998-12-28 15:47:15 +0000

    removed extraneous flush_connections() call

--- ircd/ircd.c
+++ ircd/ircd.c
@@ -19,7 +19,7 @@
  */
 
 #ifndef lint
-static  char rcsid[] = "@(#)$Id: ircd.c,v 1.38 1998/12/28 15:44:57 kalt Exp $";
+static  char rcsid[] = "@(#)$Id: ircd.c,v 1.39 1998/12/28 15:47:15 kalt Exp $";
 #endif
 
 #include "os.h"
@@ -993,7 +993,6 @@ time_t	delay;
 
 #if defined(PREFER_SERVER)
 	(void)read_message(1, &fdas);
-	flush_connections(me.fd);
 	Debug((DEBUG_DEBUG, "delay for %d", delay));
 	if (timeofday > nextc)
 	    {
