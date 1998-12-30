1b54bc04770948b314e30852f8cc3dd62ae39628
kalt <kalt>
1998-12-30 18:19:39 +0000

    make sure not to call unzip_packet() from dopacket() without data.

--- common/packet.c
+++ common/packet.c
@@ -19,7 +19,7 @@
  */
 
 #ifndef lint
-static  char rcsid[] = "@(#)$Id: packet.c,v 1.4 1998/04/05 00:14:32 kalt Exp $";
+static  char rcsid[] = "@(#)$Id: packet.c,v 1.5 1998/12/30 18:19:39 kalt Exp $";
 #endif
 
 #include "os.h"
@@ -101,7 +101,9 @@ Reg	int	length;
                         length--;
                     }   
                 cptr->flags &= ~FLAGS_ZIPSTART;
-            }   
+            }
+	if (length == 0)
+		return r;
 	if (cptr->flags & FLAGS_ZIP)
 	    {
 		/* uncompressed buffer first */
