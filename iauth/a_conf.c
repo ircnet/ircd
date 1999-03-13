15f7b0f5b2cd3796c8e4065f83b1d9799f1ea9d4
kalt <kalt>
1999-03-13 21:56:10 +0000

    added missing return statement in conf_read()

--- iauth/a_conf.c
+++ iauth/a_conf.c
@@ -18,7 +18,7 @@
  */
 
 #ifndef lint
-static  char rcsid[] = "@(#)$Id: a_conf.c,v 1.15 1999/03/11 21:04:47 kalt Exp $";
+static  char rcsid[] = "@(#)$Id: a_conf.c,v 1.16 1999/03/13 21:56:10 kalt Exp $";
 #endif
 
 #include "os.h"
@@ -395,6 +395,7 @@ char *cfile;
 	if (o_dto) *ch++ = 'T';
 	if (needh) *ch++ = 'W';
 	*ch++ = '\0';
+	return o_all;
 }
 
 /* conf_match: check if an instance is to be applied to a connection */
