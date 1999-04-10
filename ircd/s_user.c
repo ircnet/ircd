26b104079750c083a1b9ffb7bd848d0fc53b1173
kalt <kalt>
1999-04-10 16:24:28 +0000

    typo

--- ircd/s_user.c
+++ ircd/s_user.c
@@ -22,7 +22,7 @@
  */
 
 #ifndef lint
-static  char rcsid[] = "@(#)$Id: s_user.c,v 1.73 1999/03/15 20:38:35 kalt Exp $";
+static  char rcsid[] = "@(#)$Id: s_user.c,v 1.74 1999/04/10 16:24:28 kalt Exp $";
 #endif
 
 #include "os.h"
@@ -678,7 +678,7 @@ char	*nick, *username;
 ** m_nick
 **	parv[0] = sender prefix
 **	parv[1] = nickname
-** the following are only used between since version 2.9 between servers
+** the following are only used between servers since version 2.9
 **	parv[2] = hopcount
 **	parv[3] = username (login name, account)
 **	parv[4] = client host name
