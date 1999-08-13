0d6265c2b7780fa44d4eb8e7b863a64e3a98148e
kalt <kalt>
1999-08-13 17:17:42 +0000

    assume -s under CYGWIN

--- ircd/ircd.c
+++ ircd/ircd.c
@@ -19,7 +19,7 @@
  */
 
 #ifndef lint
-static  char rcsid[] = "@(#)$Id: ircd.c,v 1.61 1999/08/01 02:54:07 chopin Exp $";
+static  char rcsid[] = "@(#)$Id: ircd.c,v 1.62 1999/08/13 17:17:42 kalt Exp $";
 #endif
 
 #include "os.h"
@@ -772,6 +772,14 @@ char	*argv[];
 	if (argc > 0)
 		bad_command(); /* This exits out */
 
+#if defined(USE_IAUTH) && defined(__CYGWIN32__)
+	if ((bootopt & BOOT_NOIAUTH) == 0)
+	    {
+		bootopt |= BOOT_NOIAUTH;
+		(void)fprintf(stderr, "WARNING: Assuming -s option.\n");
+	    }
+#endif
+	
 #ifndef IRC_UID
 	if ((uid != euid) && !euid)
 	    {
