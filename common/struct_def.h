574971ea1253ac7f0e9573c48977264e633ddadd
chopin <chopin>
2003-02-15 19:25:47 +0000

    added ST_NOTUID, as !ST_UID is not what we want, according to De Morgan laws.

--- common/struct_def.h
+++ common/struct_def.h
@@ -17,7 +17,7 @@
  *   along with this program; if not, write to the Free Software
  *   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  *
- *   $Id: struct_def.h,v 1.68 2003/02/14 00:27:15 chopin Exp $
+ *   $Id: struct_def.h,v 1.69 2003/02/15 19:25:47 chopin Exp $
  */
 
 typedef	struct	ConfItem aConfItem;
@@ -839,6 +839,7 @@ typedef	struct	{
 #define	SV_OLDSQUIT	0x1000	/* server uses OLD SQUIT logic */
 
 #define	ST_UID(x)	(IsServer(x) && (x->serv->version & SV_UID))
+#define	ST_NOTUID(x)	(IsServer(x) && !(x->serv->version & SV_UID))
 
 /* used for sendto_flag */
 
