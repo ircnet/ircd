c371881602186359a831007321fa2c72686563bd
q <q>
2003-10-18 19:25:45 +0000

    Fix warnings.

--- ircd/nameser_def.h
+++ ircd/nameser_def.h
@@ -77,7 +77,7 @@
 
 /*
  *      @(#)nameser.h	8.1 (Berkeley) 6/2/93
- *	$Id: nameser_def.h,v 1.3 1998/12/13 00:19:03 kalt Exp $
+ *	$Id: nameser_def.h,v 1.4 2003/10/18 19:25:45 q Exp $
  */
 
 /*
@@ -294,7 +294,7 @@ typedef struct {
  * portable or it can be elegant but rarely both.
  */
 #define GETSHORT(s, cp) { \
-	register u_char *t_cp = (u_char *)(cp); \
+	register const u_char *t_cp = (const u_char *)(cp); \
 	(s) = ((u_int16_t)t_cp[0] << 8) \
 	    | ((u_int16_t)t_cp[1]) \
 	    ; \
@@ -302,7 +302,7 @@ typedef struct {
 }
 
 #define GETLONG(l, cp) { \
-	register u_char *t_cp = (u_char *)(cp); \
+	register const u_char *t_cp = (const u_char *)(cp); \
 	(l) = ((u_int32_t)t_cp[0] << 24) \
 	    | ((u_int32_t)t_cp[1] << 16) \
 	    | ((u_int32_t)t_cp[2] << 8) \
--- ircd/res_mkquery.c
+++ ircd/res_mkquery.c
@@ -55,7 +55,7 @@
 
 #if defined(LIBC_SCCS) && !defined(lint)
 static char sccsid[] = "@(#)res_mkquery.c	8.1 (Berkeley) 6/4/93";
-static char rcsid[] = "$Id: res_mkquery.c,v 1.5 2003/10/18 15:31:25 q Exp $";
+static char rcsid[] = "$Id: res_mkquery.c,v 1.6 2003/10/18 19:25:45 q Exp $";
 #endif /* LIBC_SCCS and not lint */
 
 #include "os.h"
@@ -136,7 +136,7 @@ int	ircd_res_mkquery(int op, const char *dname, int class, int type,
 		 * Make an additional record for completion domain.
 		 */
 		buflen -= RRFIXEDSZ;
-		n = ircd_dn_comp((char *)data, cp, buflen, dnptrs, lastdnptr);
+		n = ircd_dn_comp(data, cp, buflen, dnptrs, lastdnptr);
 		if (n < 0)
 			return (-1);
 		cp += n;
