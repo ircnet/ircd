9969c787fd7a8fc3135ef39e21fda61cb8a6784a
chopin <chopin>
2003-12-09 23:04:56 +0000

    What a mistake... trying to find_client("Target Host Addr") from C: was bound to fail. "Target Host NAME" is what we may find with find_client.

--- ircd/s_conf.c
+++ ircd/s_conf.c
@@ -48,7 +48,7 @@
  */
 
 #ifndef lint
-static  char rcsid[] = "@(#)$Id: s_conf.c,v 1.68 2003/10/18 21:33:53 chopin Exp $";
+static  char rcsid[] = "@(#)$Id: s_conf.c,v 1.69 2003/12/09 23:04:56 chopin Exp $";
 #endif
 
 #include "os.h"
@@ -950,6 +950,7 @@ int	rehash(aClient *cptr, aClient *sptr, int sig)
 	{
 		rehashed = 1;
 	}
+	srand(now);
 	return ret;
 }
 
@@ -1907,7 +1908,7 @@ aConfItem	*find_denied(char *name, int class)
 				    continue;
 			    if (!aconf2->class || ConfClass(aconf2) != ck)
 				    continue;
-			    if (find_client(aconf2->host, NULL))
+			    if (find_client(aconf2->name, NULL))
 				    return aconf2;
 			}
 		}
