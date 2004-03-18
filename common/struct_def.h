4c81df8a443a103305f39b00eadc0aa73797bd5c
chopin <chopin>
2004-03-18 01:05:29 +0000

    sigh, shouldn't have been committed

--- common/struct_def.h
+++ common/struct_def.h
@@ -17,7 +17,7 @@
  *   along with this program; if not, write to the Free Software
  *   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  *
- *   $Id: struct_def.h,v 1.92 2004/03/18 00:54:45 chopin Exp $
+ *   $Id: struct_def.h,v 1.93 2004/03/18 01:05:29 chopin Exp $
  */
 
 typedef	struct	ConfItem aConfItem;
@@ -333,7 +333,6 @@ struct	ListItem	{
 #define CFLAG_NORESOLVE		0x00010
 #define CFLAG_FALL		0x00020
 #define CFLAG_NORESOLVEMATCH	0x00040
-#define CFLAG_SERVERNAME	0x00080
 
 #define IsConfRestricted(x)	((x)->flags & CFLAG_RESTRICTED)
 #define IsConfRNoDNS(x)		((x)->flags & CFLAG_RNODNS)
@@ -342,7 +341,6 @@ struct	ListItem	{
 #define IsConfNoResolve(x)	((x)->flags & CFLAG_NORESOLVE)
 #define IsConfNoResolveMatch(x)	((x)->flags & CFLAG_NORESOLVEMATCH)
 #define IsConfFallThrough(x)	((x)->flags & CFLAG_FALL)
-#define IsConfServerName(x)	((x)->flags & CFLAG_SERVERNAME)
 
 #define	IsIllegal(x)	((x)->status & CONF_ILLEGAL)
 
