f806c9e65a340a6d13a84c13f510dda9878d3dc9
chopin <chopin>
2004-03-24 23:01:33 +0000

    even in DEBUGMODE make stats Z the same as stats z for non-opers, no need to return error NO_PRIVILEGES

--- ircd/s_serv.c
+++ ircd/s_serv.c
@@ -22,7 +22,7 @@
  */
 
 #ifndef lint
-static  char rcsid[] = "@(#)$Id: s_serv.c,v 1.179 2004/03/24 22:52:10 chopin Exp $";
+static  char rcsid[] = "@(#)$Id: s_serv.c,v 1.180 2004/03/24 23:01:33 chopin Exp $";
 #endif
 
 #include "os.h"
@@ -2196,12 +2196,10 @@ int	m_stats(aClient *cptr, aClient *sptr, int parc, char *parv[])
 		break;
 	case 'Z':
 #ifdef DEBUGMODE
-		 /* memory use (OPER only) */
+		/* memory use (OPER only) */
 		if (MyOper(sptr))
 			count_memory(cptr, parv[0], 1);
 		else
-			sendto_one(sptr, replies[ERR_NOPRIVILEGES], ME, BadTo(parv[0]));
-		break;
 #endif
 	case 'z' :	      /* memory use */
 		count_memory(cptr, parv[0], 0);
