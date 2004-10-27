9ed191f9ff780d202b20514431ba4a6cbfeb2a58
chopin <chopin>
2004-10-27 00:06:45 +0000

    send killed client user@host[serv/sid] to syslog too.

--- ircd/s_user.c
+++ ircd/s_user.c
@@ -22,7 +22,7 @@
  */
 
 #ifndef lint
-static const volatile char rcsid[] = "@(#)$Id: s_user.c,v 1.235 2004/10/09 01:28:24 chopin Exp $";
+static const volatile char rcsid[] = "@(#)$Id: s_user.c,v 1.236 2004/10/27 00:06:45 chopin Exp $";
 #endif
 
 #include "os.h"
@@ -2717,8 +2717,26 @@ int	m_kill(aClient *cptr, aClient *sptr, int parc, char *parv[])
 	}
 #if defined(USE_SYSLOG) && defined(SYSLOG_KILL)
 	if (IsOper(sptr))
-		syslog(LOG_DEBUG,"KILL From %s For %s Path %s!%s",
-			parv[0], acptr->name, inpath, path);
+	{
+		if (IsService(acptr))
+		{
+			syslog(LOG_DEBUG, "KILL From %s For %s[%s] Path %s!%s",
+				parv[0], acptr->name, 
+				isdigit(acptr->service->servp->sid[0]) ?
+				acptr->service->servp->sid : "2.10",
+				inpath, path);
+		}
+		else
+		{
+			syslog(LOG_DEBUG, "KILL From %s For %s!%s@%s[%s/%s] "
+				"Path %s!%s", parv[0], acptr->name, 
+				acptr->user->username, acptr->user->host,
+				acptr->user->servp->bcptr->name, 
+				isdigit(acptr->user->servp->sid[0]) ?
+				acptr->user->servp->sid : "2.10",
+				inpath, path);
+		}
+	}
 #endif
 	/*
 	** And pass on the message to other servers. Note, that if KILL
