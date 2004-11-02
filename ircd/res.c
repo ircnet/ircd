a808ccef7013ba870a8f04818d2a7b46fb3cd433
chopin <chopin>
2004-11-02 16:32:01 +0000

    do not show name server IP in error messages

--- ircd/res.c
+++ ircd/res.c
@@ -24,7 +24,7 @@
 #undef RES_C
 
 #ifndef lint
-static const volatile char rcsid[] = "@(#)$Id: res.c,v 1.41 2004/10/29 13:45:48 chopin Exp $";
+static const volatile char rcsid[] = "@(#)$Id: res.c,v 1.42 2004/11/02 16:32:01 chopin Exp $";
 #endif
 
 /* because there is a lot of debug code in here :-) */
@@ -937,17 +937,14 @@ struct	hostent	*get_res(char *lp)
 	    }
 	a = proc_answer(rptr, hptr, buf, buf+rc);
 	if (a == -1) {
+		sprintf(buffer, "Bad hostname returned for %s",
 #ifdef	INET6
-		sprintf(buffer, "Bad hostname returned from %s for %s",
-			inetntop(AF_INET6, &sin.sin6_addr, mydummy2, 
-				MYDUMMY_SIZE),
 			inetntop(AF_INET6, rptr->he.h_addr.s6_addr,
-				mydummy, MYDUMMY_SIZE));
+				mydummy, MYDUMMY_SIZE)
 #else
-		sprintf(buffer, "Bad hostname returned from %s for ", 
-			inetntoa((char *)&sin.sin_addr));
-		strcat(buffer, inetntoa((char *)&rptr->he.h_addr));
+			inetntoa((char *)&rptr->he.h_addr)
 #endif
+		);
 		sendto_flag(SCH_ERROR, "%s", buffer);
 		Debug((DEBUG_DNS, "%s", buffer));
 	}
