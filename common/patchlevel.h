ce0c1aa05811313d900d35a056a6563d761fb1c4
chopin <chopin>
2004-11-02 16:49:25 +0000

    2.11.0 hurray

--- common/patchlevel.h
+++ common/patchlevel.h
@@ -17,6 +17,6 @@
  */
 
 #ifndef	PATCHLEVEL
-#define PATCHLEVEL "0210991500"		/* for server identification */
+#define PATCHLEVEL "0211000000"		/* for server identification */
 #define DEVLEVEL 'b'
 #endif
