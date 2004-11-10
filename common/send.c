7743904129f1bced9a7c882008340ccae4f76941
chopin <chopin>
2004-11-10 19:08:25 +0000

    No need to increase sendq and poolsize for bursting clients when they exceed their maxsendq.

--- common/send.c
+++ common/send.c
@@ -19,7 +19,7 @@
  */
 
 #ifndef lint
-static const volatile char rcsid[] = "@(#)$Id: send.c,v 1.86 2004/11/04 15:57:41 chopin Exp $";
+static const volatile char rcsid[] = "@(#)$Id: send.c,v 1.87 2004/11/10 19:08:25 chopin Exp $";
 #endif
 
 #include "os.h"
@@ -145,20 +145,12 @@ int	send_message(aClient *to, char *msg, int len)
 # ifdef HUB
 		if (CBurst(to))
 		{
-			aConfItem	*aconf;
-
-			if (IsServer(to))
-				aconf = to->serv->nline;
-			else
-				aconf = to->confs->value.aconf;
-
-			poolsize -= MaxSendq(aconf->class) >> 1;
-			IncSendq(aconf->class);
-			poolsize += MaxSendq(aconf->class) >> 1;
+			/* Allow bursting clients (servers or services) to
+			 * exceed their maxsendq. --B. */
 			sendto_flag(SCH_NOTICE,
-				    "New poolsize %d. (sendq adjusted)",
-				    poolsize);
-			istat.is_dbufmore++;
+				"Max SendQ limit exceeded for %s: %d > %d",
+				get_client_name(to, FALSE),
+				DBufLength(&to->sendQ), get_sendq(to));
 		}
 		else
 # endif
