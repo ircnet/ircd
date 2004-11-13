79d47a6bc8557d1ae4ed25eff70efd64409dca83
chopin <chopin>
2004-11-13 16:46:18 +0000

    exit with code 1, so it's easier to script iauth config check

--- iauth/a_conf.c
+++ iauth/a_conf.c
@@ -18,7 +18,7 @@
  */
 
 #ifndef lint
-static const volatile char rcsid[] = "@(#)$Id: a_conf.c,v 1.38 2004/11/09 17:58:11 chopin Exp $";
+static const volatile char rcsid[] = "@(#)$Id: a_conf.c,v 1.39 2004/11/13 16:46:18 chopin Exp $";
 #endif
 
 #include "os.h"
@@ -42,7 +42,7 @@ static	void	conf_err(u_int nb, char *msg, char *chk)
 	else
 		sendto_log(ALOG_IRCD|ALOG_DCONF, LOG_ERR,
 			   "Configuration error line %d: %s", nb, msg);
-	exit(0);
+	exit(1);
 }
 
 /*
