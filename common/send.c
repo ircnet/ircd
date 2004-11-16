b67d686593cde5488f7351e525f4e0a2bc182c82
chopin <chopin>
2004-11-16 16:34:33 +0000

    make dead_link() accept va-args (saves us building buffers elsewhere if we want to pass more info than get_client_name)

--- common/send.c
+++ common/send.c
@@ -19,7 +19,7 @@
  */
 
 #ifndef lint
-static const volatile char rcsid[] = "@(#)$Id: send.c,v 1.90 2004/11/11 22:05:37 chopin Exp $";
+static const volatile char rcsid[] = "@(#)$Id: send.c,v 1.91 2004/11/16 16:34:33 chopin Exp $";
 #endif
 
 #include "os.h"
@@ -42,16 +42,18 @@ static	int	sentalong[MAXCONNECTIONS];
 **	Instead, mark it with FLAGS_DEADSOCK. This should
 **	generate ExitClient from the main loop.
 **
-**	If 'notice' is not NULL, it is assumed to be a format
-**	for a message to local opers. It can contain only one
-**	'%s', which will be replaced by the sockhost field of
-**	the failing link.
-**
-**	Also, the notice is skipped for "uninteresting" cases,
+**	The notice is skipped for "uninteresting" cases,
 **	like Persons and yet unknown connections...
 */
-static	int	dead_link(aClient *to, char *notice)
+static	int	dead_link(aClient *to, char *pattern, ...)
 {
+	char	notice[BUFSIZE];
+	va_list	va;
+
+	va_start(va, pattern);
+	vsprintf(notice, pattern, va);
+	va_end(va);
+
 	SetDead(to);
 	/*
 	 * If because of BUFFERPOOL problem then clean dbufs now so that
@@ -60,8 +62,8 @@ static	int	dead_link(aClient *to, char *notice)
 	DBufClear(&to->recvQ);
 	DBufClear(&to->sendQ);
 	if (!IsPerson(to) && !IsUnknown(to) && !(to->flags & FLAGS_CLOSING))
-		sendto_flag(SCH_ERROR, notice, get_client_name(to, FALSE));
-	Debug((DEBUG_ERROR, notice, get_client_name(to, FALSE)));
+		sendto_flag(SCH_ERROR, notice);
+	Debug((DEBUG_ERROR, notice));
 	return -1;
 }
 
@@ -155,19 +157,15 @@ int	send_message(aClient *to, char *msg, int len)
 		else
 # endif
 		{
-			char ebuf[BUFSIZE];
-
-			ebuf[0] = '\0';
+			to->exitc = EXITC_SENDQ;
 			if (IsService(to) || IsServer(to))
 			{
-				sprintf(ebuf,
+				return dead_link(to,
 				"Max SendQ limit exceeded for %s: %d > %d",
 					get_client_name(to, FALSE),
 					DBufLength(&to->sendQ), get_sendq(to));
 			}
-			to->exitc = EXITC_SENDQ;
-			return dead_link(to, ebuf[0] ? ebuf :
-				"Max Sendq exceeded");
+			return dead_link(to, "Max Sendq exceeded");
 		}
 	}
 # ifdef	ZIP_LINKS
@@ -215,7 +213,8 @@ tryagain:
 		{
 			to->exitc = EXITC_MBUF;
 			return dead_link(to,
-				"Buffer allocation error for %s");
+				"Buffer allocation error for %s",
+				get_client_name(to, FALSE));
 		}
 	}
 	/*
@@ -286,7 +285,8 @@ int	send_queued(aClient *to)
 			    {
 				to->exitc = EXITC_MBUF;
 				return dead_link(to,
-					 "Buffer allocation error for %s");
+					 "Buffer allocation error for %s",
+					get_client_name(to, FALSE));
 			    }
 		    }
 	    }
@@ -308,7 +308,8 @@ int	send_queued(aClient *to)
 					ME, bysptr->name, to->name);
 				}
 			}
-			return dead_link(to,"Write error to %s, closing link");
+			return dead_link(to, "Write error to %s, closing link",
+				get_client_name(to, FALSE));
 		}
 		(void)dbuf_delete(&to->sendQ, rlen);
 		to->lastsq = DBufLength(&to->sendQ)/1024;
@@ -333,7 +334,8 @@ int	send_queued(aClient *to)
 			    {
 				to->exitc = EXITC_MBUF;
 				return dead_link(to,
-					 "Buffer allocation error for %s");
+					 "Buffer allocation error for %s",
+					get_client_name(to, FALSE));
 			    }
 		    }
 #endif
