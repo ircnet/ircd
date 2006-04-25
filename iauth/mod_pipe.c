59c1666d2eed9121e74157b74eb140d82b6a6abc
chopin <chopin>
2006-04-25 21:45:01 +0000

    execlp wants chars, not integers

--- iauth/mod_pipe.c
+++ iauth/mod_pipe.c
@@ -18,7 +18,7 @@
  */
 
 #ifndef lint
-static const volatile char rcsid[] = "@(#)$Id: mod_pipe.c,v 1.6 2005/01/03 15:36:21 q Exp $";
+static const volatile char rcsid[] = "@(#)$Id: mod_pipe.c,v 1.7 2006/04/25 21:45:01 chopin Exp $";
 #endif
 
 #include "os.h"
@@ -95,6 +95,8 @@ static	int	pipe_start(u_int cl)
 		    return -1;
 	    case 0 :
 		    {
+			char portbuf[6];	/* 5 chars to hold the port number */
+
 			(void)close(pp[0]);
 			for (rc = 2; rc < MAXCONNECTIONS; rc++)
 				if (rc != pp[1])
@@ -104,9 +106,10 @@ static	int	pipe_start(u_int cl)
 			(void)dup2(2, 1);
 			if (pp[1] != 2 && pp[1] != 1)
 				(void)close(pp[1]);
+			snprintf(portbuf, 5, "%d", cldata[cl].itsport);
 			(void)execlp(cldata[cl].instance->popt,
 				cldata[cl].instance->popt,
-				cldata[cl].itsip, cldata[cl].itsport,
+				cldata[cl].itsip, portbuf,
 				(char *) NULL);
 			_exit(-1);
 		    }
