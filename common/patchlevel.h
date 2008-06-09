f5f51304d527398ee97a36abaacc097db5e100ce
chopin <chopin>
2008-06-09 21:04:30 +0000

    details and release

--- common/patchlevel.h
+++ common/patchlevel.h
@@ -17,6 +17,6 @@
  */
 
 #ifndef	PATCHLEVEL
-#define PATCHLEVEL "0211010200"		/* for server identification */
-#define DEVLEVEL 'a'
+#define PATCHLEVEL "0211010100"		/* for server identification */
+#define DEVLEVEL 'b'
 #endif
