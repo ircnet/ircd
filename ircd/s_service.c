bd852f4e8c005fd2ef8825e99a188eac9eabdbb3
Patrick <patrick@ircnet.com>
2026-04-19 16:33:48 +0200

    Services: Added SASL username to UNICK

--- ircd/s_service.c
+++ ircd/s_service.c
@@ -197,7 +197,7 @@ static	void	sendnum_toone(aClient *cptr, int wants, aClient *sptr,
 		umode = "+";
 
 	if ((wants & SERVICE_WANT_UID) && sptr->user)
-		sendto_one(cptr, ":%s UNICK %s %s %s %s %s %s :%s",
+		sendto_one(cptr, ":%s UNICK %s %s %s %s %s %s %s :%s",
 			sptr->user->servp->sid,
 			(wants & SERVICE_WANT_NICK) ? sptr->name : ".",
 			sptr->uid,
@@ -205,6 +205,7 @@ static	void	sendnum_toone(aClient *cptr, int wants, aClient *sptr,
 			(wants & SERVICE_WANT_USER) ? sptr->user->host : ".",
 			(wants & SERVICE_WANT_USER) ? get_client_ip(sptr) : ".",
 			(wants & (SERVICE_WANT_UMODE|SERVICE_WANT_OPER)) ? umode : "+",
+ 		    (wants & SERVICE_WANT_USER) ? (IsSASLAuthed(sptr) ? sptr->sasl_user : "*") : "",
 			(wants & SERVICE_WANT_USER) ? sptr->info : "");
 	else
 	if (wants & SERVICE_WANT_EXTNICK)
