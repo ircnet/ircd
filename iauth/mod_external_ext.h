010053e8e99c6cf0508cdfcc75d276881c2b6ee3
patrick <146866967+patrick-irc@users.noreply.github.com>
2026-08-29 01:05:04 +0200

    Added iauth mod_external (#88)
    * Added iauth mod_external

---
+++ iauth/mod_external_ext.h
@@ -0,0 +1,29 @@
+/************************************************************************
+*   IRC - Internet Relay Chat, iauth/mod_external_ext.h
+*   Copyright (C) 2025 IRCnet.com team
+*
+*   This program is free software; you can redistribute it and/or modify
+*   it under the terms of the GNU General Public License as published by
+*   the Free Software Foundation; either version 1, or (at your option)
+*   any later version.
+*
+*   This program is distributed in the hope that it will be useful,
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*   GNU General Public License for more details.
+*
+*   You should have received a copy of the GNU General Public License
+*   along with this program; if not, write to the Free Software
+*   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
+/*  This file contains external definitions for global variables and functions
+*  defined in iauth/mod_external.c.
+*/
+
+/*
+* External definitions for global variables.
+*/
+#ifndef MOD_EXTERNAL_C
+extern aModule Module_external;
+#endif
